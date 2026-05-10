0458f3730530b584c736e031c47b8c184066c61c
Tomasz Śmigielski <tsmigiel@ik.me>
2026-05-10 13:58:35 +0200

    WineRelayHost

---
+++ WineRelayHost/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Diagnostics;
+
+using var wineRelayProcess = new Process();
+wineRelayProcess.StartInfo = new ("protontricks-launch", "--appid 2537590 /home/ts-pl/Projects/dotnet/MsFsStreamDeckConnector/WineRelay/bin/Debug/net10.0/win-x64/WineRelay.exe");
+wineRelayProcess.Start();
